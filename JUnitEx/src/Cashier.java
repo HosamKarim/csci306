e55b1bbdde3853c24fc9ff92967774f4f4ba4445
HosamMohd <hosamkareem@gmail.com>
2012-09-30 16:39:57 -0600

    Cashier Class >> makechanges method -- main method for testing

--- JUnitEx/src/Cashier.java
+++ JUnitEx/src/Cashier.java
@@ -1,7 +1,41 @@
 
 public class Cashier {
 	
-	public int makeChange(Envelope en, int price){
-		return new Envelope().getDollars(); 
+	public Envelope makeChange(int numOfD, int numOfC){
+		Envelope env = new Envelope();
+		int qua;
+		int dim;
+		int ni;
+		
+		env.setDollars(numOfD);
+		
+		qua = (int)(numOfC/25);
+		env.setQuarter(qua);
+		numOfC -= qua*25;
+		
+		dim = (int)(numOfC/10);
+		env.setDimes(dim);
+		numOfC -= dim*10;
+		
+		ni = (int)(numOfC/5);
+		env.setNickels(ni);
+		numOfC -= ni*5;
+
+		env.setPennies(numOfC);
+		
+		return env; 
+	}
+	
+	//TEST
+	public static void main(String[] args) {
+		Cashier cc = new Cashier();
+		Envelope ee = cc.makeChange(222,68);
+		
+		System.out.println(ee.getDollars());
+		System.out.println(ee.getQuarter());
+		System.out.println(ee.getDimes());
+		System.out.println(ee.getNickels());
+		System.out.println(ee.getPennies());
+		
 	}
 }
